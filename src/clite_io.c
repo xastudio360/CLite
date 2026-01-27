3833e93451aa077a727370c7afb9b0d5eebc8102
XA Studio <xa.studio.360@gmail.com>
2026-01-27 18:14:41 +0700

    Refactor input/output functions in clite_io.c

--- src/clite_io.c
+++ src/clite_io.c
@@ -1,9 +1,9 @@
 /**
  * @file clite_io.c
- * @brief Implementation of CLite input/output functions
+ * @brief CLite I/O functions implementation
  */
 
-#include "../include/clite/clite_io.h"
+#include "clite_io.h"
 #include <string.h>
 
 void print(const char* str) {
@@ -82,10 +82,144 @@ float scan_float(void) {
     return value;
 }
 
+double scan_double(void) {
+    double value;
+    while (scanf("%lf", &value) != 1) {
+        // Clear buffer on input error
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    // Clear remaining buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+    return value;
+}
+
+signed char scan_byte(void) {
+    int temp;
+    while (scanf("%d", &temp) != 1 || temp < -128 || temp > 127) {
+        // Clear buffer on input error
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    // Clear remaining buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+    return (signed char)temp;
+}
+
+unsigned char scan_ubyte(void) {
+    unsigned int temp;
+    while (scanf("%u", &temp) != 1 || temp > 255) {
+        // Clear buffer on input error
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    // Clear remaining buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+    return (unsigned char)temp;
+}
+
+short scan_short(void) {
+    short value;
+    while (scanf("%hd", &value) != 1) {
+        // Clear buffer on input error
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    // Clear remaining buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+    return value;
+}
+
+unsigned short scan_ushort(void) {
+    unsigned short value;
+    while (scanf("%hu", &value) != 1) {
+        // Clear buffer on input error
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    // Clear remaining buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+    return value;
+}
+
+unsigned int scan_uint(void) {
+    unsigned int value;
+    while (scanf("%u", &value) != 1) {
+        // Clear buffer on input error
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    // Clear remaining buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+    return value;
+}
+
+long scan_long(void) {
+    long value;
+    while (scanf("%ld", &value) != 1) {
+        // Clear buffer on input error
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    // Clear remaining buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+    return value;
+}
+
+unsigned long scan_ulong(void) {
+    unsigned long value;
+    while (scanf("%lu", &value) != 1) {
+        // Clear buffer on input error
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    // Clear remaining buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+    return value;
+}
+
+char* scan_line_safe(char* buffer, int size) {
+    return scan_line(buffer, size);
+}
+
+void print_error(const char* str) {
+    if (str == NULL) return;
+    fprintf(stderr, "%s", str);
+    fflush(stderr);
+}
+
+void printfl_error(const char* format, ...) {
+    if (format == NULL) return;
+    
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    
+    fprintf(stderr, "\n");
+    fflush(stderr);
+}
+
 int scanfp(const char* prompt, const char* format, ...) {
     if (prompt == NULL || format == NULL) return 0;
     
-    // Display prompt
+    // Print prompt
     printf("%s", prompt);
     fflush(stdout);
     
